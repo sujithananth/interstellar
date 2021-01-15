4b626b38432e00754f95ce01137ac9eba35aacf0
sujith ananth <www.sujithananth20@gmail.com>
2021-01-15 00:21:13 -0800

    initial commit

--- travel.py
+++ travel.py
@@ -4,16 +4,16 @@ from random import randint
 from pygame import mixer  #module for playing music  
 x=(53,81,93)  #blue colour for atmosphere sky blue
 Z=(0,0,0)     #black colour for space 
-mixer.init()  #initializing the module mixer
-mixer.music.load('C:\Program Files\Python38-32\code\interstellar\music\walker.mp3')
+#mixer.init()  #initializing the module mixer
+#mixer.music.load('C:\Users\pc\code\interstellar\music\walker.mp3')
 #loading the music i edited the countdown voice and song and made it one 
-mixer.music.play() #playing it
+#mixer.music.play() #playing it
 
 score=0      
 HEIGHT = 500
 WIDTH  =700
 
-rocket= Actor("rocket3")
+rocket= Actor("rocket")
 rocket.bottom =HEIGHT 
 rocket.x = WIDTH//2
 
